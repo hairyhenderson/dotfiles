2ae4748f2c0832748a6e575eb929d34f2c727bf3
Dave Henderson <David.Henderson@qlik.com>
2016-04-05 21:32:08 -0400

    Linking first to rcrc

--- setup.sh
+++ setup.sh
@@ -4,5 +4,8 @@ set -e
 # make sure submodules are pulled down too
 git submodule update --init --recursive
 
+# Make sure rcm's config is set up
+ln -s $HOME/.dotfiles/rcrc $HOME/.rcrc
+
 # set up symlinks
-rcup
+rcup 
