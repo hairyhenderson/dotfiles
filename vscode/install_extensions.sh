38fce9d2c09e9148000fb7e41b9499473f643131
Dave Henderson <dhenderson@gmail.com>
2021-06-17 08:27:57 -0400

    Updates
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -14,6 +14,7 @@ code --install-extension efbenson.scad
 code --install-extension esbenp.prettier-vscode
 code --install-extension felipe.nasc-touchbar
 code --install-extension formulahendry.github-actions
+code --install-extension GitHub.codespaces
 code --install-extension GitHub.vscode-codeql
 code --install-extension GitHub.vscode-pull-request-github
 code --install-extension golang.go
@@ -26,9 +27,11 @@ code --install-extension James-Yu.latex-workshop
 code --install-extension jock.svg
 code --install-extension kamikillerto.vscode-colorize
 code --install-extension michelemelluso.code-beautifier
+code --install-extension mindaro-dev.file-downloader
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
 code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
 code --install-extension ms-toolsai.jupyter
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
@@ -37,8 +40,10 @@ code --install-extension ms-vscode-remote.remote-wsl
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension ms-vscode.azure-account
 code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.test-adapter-converter
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension oderwat.indent-rainbow
+code --install-extension redhat.vscode-commons
 code --install-extension redhat.vscode-yaml
 code --install-extension ryanluker.vscode-coverage-gutters
 code --install-extension slevesque.vscode-hexdump
