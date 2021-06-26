a8507589bcd4eb959e2c24cb48a5473bcd12015c
Dave Henderson <dhenderson@gmail.com>
2021-06-26 11:12:51 -0400

    trying something...
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ pushd $HOME/.dotfiles
 git submodule update --init --recursive
 popd
 
-# Make sure rcm's config is set up
-ln -s $HOME/.dotfiles/.rcrc $HOME/.rcrc || true
+# # Make sure rcm's config is set up
+# ln -s $HOME/.dotfiles/.rcrc $HOME/.rcrc || true
 
 # set up symlinks
 cd $HOME
-rcup 
+RCRC=$HOME/.dotfiles/.rcrc rcup
 
 # Other setup
 if [ $(uname -o) = "Darwin" ]; then
