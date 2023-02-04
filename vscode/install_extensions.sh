cc47195ceecefe14d569418a1ffba60ea881992c
Dave Henderson <dhenderson@gmail.com>
2023-02-04 15:19:47 -0500

    updates
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -3,8 +3,10 @@ code --install-extension adambaldwin.vscode-nsp
 code --install-extension aeschli.vscode-css-formatter
 code --install-extension akmittal.hugofy
 code --install-extension Antyos.openscad
+code --install-extension bbenoist.Nix
 code --install-extension be5invis.toml
 code --install-extension bierner.markdown-mermaid
+code --install-extension Cardinal90.multi-cursor-case-preserve
 code --install-extension carolynvs.dep
 code --install-extension casualjim.gotemplate
 code --install-extension chenxsan.vscode-standardjs
@@ -33,11 +35,9 @@ code --install-extension michelemelluso.code-beautifier
 code --install-extension mindaro-dev.file-downloader
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension ms-python.isort
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
-code --install-extension ms-toolsai.jupyter
-code --install-extension ms-toolsai.jupyter-keymap
-code --install-extension ms-toolsai.jupyter-renderers
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
@@ -46,6 +46,7 @@ code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension ms-vscode.azure-account
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.live-server
+code --install-extension ms-vscode.remote-explorer
 code --install-extension ms-vscode.test-adapter-converter
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension mtxr.sqltools
